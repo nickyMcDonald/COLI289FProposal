a49d985b4ff59268457515ffd1bb1d176d5c2a8d
nickyMcDonald <namcdon@outlook.com>
2025-12-31 09:59:43 -0500

    Update rules.h

--- src/rules.h
+++ src/rules.h
@@ -1,6 +1,68 @@
 #pragma once
 #include "def.h"
 
+/// @brief Checks if a player was won
+/// @param cells The board
+/// @param player The last player to move
+/// @return The winning player
+State check_win(const State cells[GAME_WIDTH][GAME_WIDTH], State player)
+{
+    // Check for straight win
+    for (int i = 0; i < GAME_WIDTH; i++)
+    {
+        bool isWinX = cells[i][0] & STATE_XO;
+        bool isWinY = cells[0][i] & STATE_XO;
+        for (int j = 1; j < GAME_WIDTH; j++)
+        {
+            if (cells[i][0] != cells[i][j])
+            {
+                isWinX = false;
+            }
+            if (cells[0][i] != cells[j][i])
+            {
+                isWinY = false;
+            }
+        }
+        if (isWinX || isWinY)
+        {
+            return player;
+        }
+    }
+
+    // Check for diagonal win
+    bool isWinDown = cells[0][0] & STATE_XO;
+    bool isWinUp = cells[0][GAME_WIDTH - 1] & STATE_XO;
+    for (int i = 1; i < GAME_WIDTH; i++)
+    {
+        if (cells[i][i] != cells[0][0])
+        {
+            isWinDown = false;
+        }
+        if (cells[i][GAME_WIDTH - 1 - i] != cells[0][GAME_WIDTH - 1])
+        {
+            isWinUp = false;
+        }
+    }
+    if (isWinDown || isWinUp)
+    {
+        return player;
+    }
+    
+    // Check for draw
+    player = STATE_DRAW;
+    for (int y = 0; y < GAME_WIDTH; y++)
+    {
+        for (int x = 0; x < GAME_WIDTH; x++)
+        {
+            if (!(cells[x][y] & STATE_XO))
+            {
+                player = STATE_NONE;
+            }
+        }
+    }
+    return player;
+}
+
 /// @brief Evaluate a proposed move and determine if it results in a win or draw
 /// @param cells The current game state
 /// @param move The proposed move
@@ -28,65 +90,7 @@ State evaluate_move(const State cells[GAME_WIDTH][GAME_WIDTH], Point move, State
             }
         }
         c[move.x][move.y] = player;
-        
-
-        // Check for straight win
-        for (int i = 0; i < GAME_WIDTH; i++)
-        {
-            bool isWinX = c[i][0] & STATE_XO;
-            bool isWinY = c[0][i] & STATE_XO;
-            for (int j = 1; j < GAME_WIDTH; j++)
-            {
-                if (c[i][0] != c[i][j])
-                {
-                    isWinX = false;
-                }
-                if (c[0][i] != c[j][i])
-                {
-                    isWinY = false;
-                }
-            }
-            if (isWinX || isWinY)
-            {
-                return player;
-            }
-        }
-
-        // Check for diagonal win
-        bool isWinDown = c[0][0] & STATE_XO;
-        bool isWinUp = c[0][GAME_WIDTH - 1] & STATE_XO;
-        for (int i = 1; i < GAME_WIDTH; i++)
-        {
-            if (c[i][i] != c[0][0])
-            {
-                isWinDown = false;
-            }
-            if (c[i][GAME_WIDTH - 1 - i] != c[0][GAME_WIDTH - 1])
-            {
-                isWinUp = false;
-            }
-        }
-        if (isWinDown || isWinUp)
-        {
-            return player;
-        }
-        
-        // Check for draw
-        player = STATE_DRAW;
-        for (int y = 0; y < GAME_WIDTH; y++)
-        {
-            for (int x = 0; x < GAME_WIDTH; x++)
-            {
-                if (!(c[x][y] & STATE_XO))
-                {
-                    player = STATE_NONE;
-                }
-            }
-        }
-        if (player == STATE_DRAW)
-        {
-            return player;
-        }
+        return check_win(c, player);
     }
     return STATE_NONE;
 }
